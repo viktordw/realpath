665e05871f8cbc6c0d96e74f7ac84c44a418e33e
Viktor <viktor.pavlovic@protonmail.com>
2021-12-31 15:58:46 +0100

    Prvi doprinos

---
+++ includes/hello.sh
@@ -0,0 +1,3 @@
+function hello () {
+printf "Putanja do skripte hello je: \n  ${SCRIPTPATH}/includes/hello.sh\n"
+}
---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export SCRIPT=`realpath $0`
+export SCRIPTPATH=`dirname $SCRIPT`
+
+function divider () {
+printf '=%.0s' {1..50} && printf '\n'
+}
+source "${SCRIPTPATH}/includes/hello.sh"
+
+path=$(pwd)
+divider
+printf "Trenutna putanja je:\n  ${path}\n"
+printf "Putanja do glavnog fajla je:\n  ${SCRIPT}\n"
+hello
+divider
