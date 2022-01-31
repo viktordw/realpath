e8ffa4aa239c9e1290173e430673c423a93f7f43
viktordw <viktor@dreamweb.rs>
2022-01-31 15:07:09 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ export SCRIPT=`realpath $0`
 export SCRIPTPATH=`dirname $SCRIPT`
 
 function divider () {
-printf '=%.0s' {1..50} && printf '\n'
+  printf '=%.0s' {1..50} && printf '\n'
 }
 source "${SCRIPTPATH}/includes/hello.sh"
 
